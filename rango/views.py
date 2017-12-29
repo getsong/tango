5a0c7c65dce1861d40c3bc4ae395530d260becf6
getsong <getsong13@gmail.com>
2017-12-28 22:26:35 -0500

    added about page

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Rango!")
+    html = "Hello Rango!<br>" \
+           "Click <a href='/rango/about/'>here</a> to go to the about page"
+    return HttpResponse(html)
+
+def about(request):
+    html = "Rango says here is the about page<br>" \
+             "Click <a href='/rango/index/'>here</a> to go to the index page"
+    return HttpResponse(html)
